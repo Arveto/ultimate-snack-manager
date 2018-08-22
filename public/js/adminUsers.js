3bd984919f1af8a61986351270d3f33557f94447
Jésus Brakmar <31973542+TERRUSS@users.noreply.github.com>
2018-08-23 01:31:58 +0200

    Think it's good

--- public/js/adminUsers.js
+++ public/js/adminUsers.js
@@ -166,7 +166,7 @@ function insertUserAdmin(div, user){
     div.append('<article class="media adminUser'+user.id+'">\
     <figure class="media-left">\
         <p class="image is-48x48">\
-            <img src="https://bulma.io/images/placeholders/128x128.png">\
+            <img src="https://api.adorable.io/avatars/100/'+user.faName+user.fiName+'.png">\
         </p>\
     </figure>\
     <div class="media-content columns">\
