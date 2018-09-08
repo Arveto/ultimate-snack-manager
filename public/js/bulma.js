1b8b834762325b3352a4865f39a152efb09f1d7d
PurplePachyderm <a.delval87@gmail.com>
2018-09-08 17:16:44 +0200

    Set up prod version (minified JS/CSS and Babel)

--- public/js/bulma.js
+++ public/js/bulma.js
@@ -1,10 +1,1 @@
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#' + burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
+(function(){var a=document.querySelector(".burger"),b=document.querySelector("#"+a.dataset.target);a.addEventListener("click",function(){a.classList.toggle("is-active"),b.classList.toggle("is-active")})})();
--- public/js/shoppingList.js
+++ public/js/shoppingList.js
@@ -1,71 +1,11 @@
-
-function shoppingListAddProduct(){
-    let newProduct = {};
-    newProduct.content = $('#shoppingListInput').val();
-
-    socket.emit('shoppingListAddProduct', newProduct);
-
-    $('#shoppingListInput').val('');
-}
-
-function shoppingListProductEdit(item){ //Ooooh yeah sooo dirty.
-    item.content = $('#shoppingContent'+item.id).text();
-
-    $('#shoppingContent'+item.id).html('<input id="shoppingInput'+item.id+'"></input>');
-
-    $("#shoppingInput"+item.id).val(item.content);
-
-    $("#shoppingInput"+item.id).on('keypress', (e)=>{
-        let keycode = e.keyCode || e.which;
-        if (keycode == 13) {
-            socket.emit('shoppingListProductEdit', {'content': $("#shoppingInput"+item.id).val(), 'id': item.id});
-        }
-    });
-}
-
-function shoppingListCheckProduct(item){
-    socket.emit('shoppingListDeleteProduct', (item));
-}
-
-
-
-
-//**************************************************
-// UI EVENTS
-
-$('#shoppingListInput').on('keypress', (e)=>{
-    let keycode = e.keyCode || e.which;
-    if (keycode == 13) {
-        shoppingListAddProduct();
-    }
-});
-
-$('#button_shoppingListAddProduct').on('click', ()=>{ // TODO: Fix that shit : cannot click on the button
-    shoppingListAddProduct();
-});
-
-//**************************************************
-// SOCKET EVENTS
-
-socket.on('shoppingListProductEdit', (item)=>{
-    $('#shoppingContent'+item.id).html(item.content);
-});
-
-socket.on('shoppingListDeleteProduct', (item)=>{
-    $('#shoppingProduct'+item.id).remove();
-});
-
-socket.on('shoppingListAddProduct', (item)=>{
-
-    $('#shoppingListContainer').append(`<tr class="has-icons-right" id="shoppingProduct`+item.id+`">\
-        <td id="shoppingContent`+item.id+`"> `+item.content+` </td>\
+function shoppingListAddProduct(){let a={};a.content=$("#shoppingListInput").val(),socket.emit("shoppingListAddProduct",a),$("#shoppingListInput").val("")}function shoppingListProductEdit(a){a.content=$("#shoppingContent"+a.id).text(),$("#shoppingContent"+a.id).html("<input id=\"shoppingInput"+a.id+"\"></input>"),$("#shoppingInput"+a.id).val(a.content),$("#shoppingInput"+a.id).on("keypress",b=>{let c=b.keyCode||b.which;13==c&&socket.emit("shoppingListProductEdit",{content:$("#shoppingInput"+a.id).val(),id:a.id})})}function shoppingListCheckProduct(a){socket.emit("shoppingListDeleteProduct",a)}$("#shoppingListInput").on("keypress",a=>{let b=a.keyCode||a.which;13==b&&shoppingListAddProduct()}),$("#button_shoppingListAddProduct").on("click",()=>{shoppingListAddProduct()}),socket.on("shoppingListProductEdit",a=>{$("#shoppingContent"+a.id).html(a.content)}),socket.on("shoppingListDeleteProduct",a=>{$("#shoppingProduct"+a.id).remove()}),socket.on("shoppingListAddProduct",a=>{$("#shoppingListContainer").append(`<tr class="has-icons-right" id="shoppingProduct`+a.id+`">\
+        <td id="shoppingContent`+a.id+`"> `+a.content+` </td>\
             <td class="icon is-right">\
-                <a class= "button is-small is-info" onclick="shoppingListProductEdit( {name:'`+item.content+`', id:`+item.id+`})" href="#">\
+                <a class= "button is-small is-info" onclick="shoppingListProductEdit( {name:'`+a.content+`', id:`+a.id+`})" href="#">\
                     <i class="fa fa-edit "></i>\
                 </a>\
-                <a class= "button is-small is-success" onclick="shoppingListCheckProduct( {name:'`+item.content+`', id:`+item.id+`})" href="#">\
+                <a class= "button is-small is-success" onclick="shoppingListCheckProduct( {name:'`+a.content+`', id:`+a.id+`})" href="#">\
                     <i class="fa fa-check "></i>\
                 </a>\
         </td>\
-        </tr>`).appendTo('#shoppingListProducts');
-});
+        </tr>`).appendTo("#shoppingListProducts")});
